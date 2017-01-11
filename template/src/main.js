5157835e791fe329ed410367c673924105fa9ba4
airyland <airyland@qq.com>
2017-01-11 08:57:07 +0800

    clean template

--- template/src/main.js
+++ template/src/main.js
@@ -24,12 +24,6 @@ FastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  {{#if_eq build "runtime"}}
   render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
-  {{#if_eq build "standalone"}}
-  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
 }).$mount('#app'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
